308e4e839e2c93a4872c060a4a06dc0bd8b61766
Ed <jr-lol@live.com>
2020-08-19 18:48:06 -0300

    challenge-28

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -1,3 +1,6 @@
+(function () {
+  "use strict";
+
   /*
   No HTML:
   - Crie um formulário com um input de texto que receberá um CEP e um botão
@@ -25,3 +28,47 @@
   - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
   adicionar as informações em tela.
   */
+  var $cep = document.querySelector('[data-js="cep"]');
+  var $logradouro = document.querySelector('[data-js="logradouro"');
+  var $bairro = document.querySelector('[data-js="bairro"]');
+  var $cidade = document.querySelector('[data-js="cidade"]');
+  var $estado = document.querySelector('[data-js="estado"]');
+  var $msg = document.querySelector('[data-js="msg"]');
+
+  var state = {
+    0: "Não enviado",
+    1: "Conexão aberta",
+    2: "Headers recebidos",
+    3: "Carregando conteúdo",
+    4: "Concluído",
+  };
+
+  var xhr = new XMLHttpRequest();
+  var $submit = document.querySelector('[data-js="submit"]');
+  $submit.addEventListener("click", function (e) {
+    var firstPart = $cep.value.match(/\d/g).slice(0, 5).join("");
+    var lastPart = $cep.value.match(/\d/g).slice(-3).join("");
+    $msg.innerHTML = state[xhr.readyState];
+    xhr.open("GET", `https://ws.apicep.com/cep/${firstPart}${lastPart}.json`);
+    $msg.innerHTML = state[xhr.readyState];
+    xhr.send();
+    $msg.innerHTML = state[xhr.readyState];
+  });
+
+  xhr.addEventListener("readystatechange", function () {
+    if (xhr.status === 200 && xhr.readyState === 4) {
+      var info = JSON.parse(xhr.responseText);
+
+      if (info.status === 200) {
+        $msg.innerHTML = `Endereço referente ao CEP: ${$cep.value}`;
+
+        $logradouro.value = info.address;
+        $bairro.value = info.district;
+        $cidade.value = info.city;
+        $estado.value = info.state;
+      } else {
+        $msg.innerHTML = `Não encontramos dados para o cep: ${$cep.value}`;
+      }
+    }
+  });
+})();
